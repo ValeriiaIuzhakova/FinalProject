79325a8633bd107e3c96a77ec01e510e978eb049
Anna Milash <milashannas@gmail.com>
2016-02-02 00:28:38 +0200

    update

--- FinalProject/src/com/company/Factorial.java
+++ FinalProject/src/com/company/Factorial.java
@@ -1,12 +1,15 @@
-
+package com.company;
 
 public class Factorial {
+    public static long factorialCalculate(int n) {
 
-    public static long factorialCalculate(int num) {
-        int fact=1;
-
-        for (; num > 0; fact *= num--);
-
-        return fact;
+        int result = 1;
+        if (n == 0 || n == 1)
+            return result;
+        else {
+            for (int i = 2; i <= n; i++)
+               result *= i;
+            return result;
+        }
     }
 }
