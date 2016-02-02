8a805558a9843a26f70140be6a6204668f639052
Valeriia Iuzhakova <sv2007g@gmail.com>
2016-02-02 20:13:40 +0200

    Final project update

--- FinalProject/src/com/company/Factorial.java
+++ FinalProject/src/com/company/Factorial.java
@@ -9,7 +9,7 @@ public class Factorial {
         else {
             for (int i = 2; i <= n; i++)
                 result *= i;
-                return result;
+            return result;
         }
     }
 }
